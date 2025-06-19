3ae3cf1ab90b4209f7d6c7ebf7e72c3f71bc5f60
Thomas Dragoi <thomasdragoi@yahoo.de>
2025-06-19 21:28:55 +0200

    Success Spec2Fail0

--- Electrons/spec/electrons_spec.js
+++ Electrons/spec/electrons_spec.js
@@ -2,9 +2,8 @@ describe("Electrons around the cores", function() {
   it("should return 6 for [1,2,3,4,5]", function() {
     expect(electrons([1,2,3,4,5])).toBe(6);
   });
-  
-  it("should return 4 for [2,2,3,3]", function() {
-  expect(electrons([2,2,3,3])).toBe(4);
-});
 
+  it("should return 4 for [2,2,3,3]", function() {
+    expect(electrons([2,2,3,3])).toBe(4);
+  });
 });
--- Electrons/src/electrons.js
+++ Electrons/src/electrons.js
@@ -1,3 +1,16 @@
 function electrons(dice) {
-  return 6;
+  let total = 0;
+  for (const roll of dice) {
+    switch (roll) {
+      case 3:
+        total += 2;
+        break;
+      case 5:
+        total += 4;
+        break;
+      default:
+        break;
+    }
+  }
+  return total;
 }
