a1d607fb51156881f3d5cf88ef313b9f14986315
Thomas Dragoi <thomasdragoi@yahoo.de>
2025-06-19 21:37:40 +0200

    Five Test Cases

--- Electrons/spec/electrons_spec.js
+++ Electrons/spec/electrons_spec.js
@@ -6,4 +6,17 @@ describe("Electrons around the cores", function() {
   it("should return 4 for [2,2,3,3]", function() {
     expect(electrons([2,2,3,3])).toBe(4);
   });
+  
+  it("should return 2 for [6,6,4,4,1,3]", function() {
+  expect(electrons([6,6,4,4,1,3])).toBe(2);
+  });
+
+  it("should return 12 for [3,5,3,5,4,2]", function() {
+  expect(electrons([3,5,3,5,4,2])).toBe(12);
+  });
+
+  it("should return 8 for [5, 5, 1, 1, 2, 2]", function() {
+  expect(electrons([5, 5, 1, 1, 2, 2])).toBe(8);
+  });
+
 });
